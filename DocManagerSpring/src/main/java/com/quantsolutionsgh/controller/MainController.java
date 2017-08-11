f2ef7297d3758e028174ed99721dd758462a11e0
bernard <bernard@DBA-PC>
2017-08-11 16:31:28 +0000

    Enabled  cross origin request in MainController
    Enabled cross origin request and also  corrected the
    spelling of verifiction in viewer.jsp

--- DocManagerSpring/src/main/java/com/quantsolutionsgh/controller/MainController.java
+++ DocManagerSpring/src/main/java/com/quantsolutionsgh/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,7 @@ import com.quantsolutionsgh.model.StringResponse;
 import com.quantsolutionsgh.model.UploadedFile;
 
 @Controller
+@CrossOrigin
 public class MainController {
 
 	private IDocManagerMethods docMan;
